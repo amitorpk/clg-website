c23106190182269d0b5dbedbe1ecef144405abde
amitorpk <pradeep2019@iiitkottayam.ac.in>
2023-11-23 00:14:23 +0530

    check2

--- script.js
+++ script.js
@@ -34,47 +34,91 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-let prev = document.getElementById ('prev');
-let next = document.getElementById ('next');
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
 
-next.addEventListener ("click", function () {
-  showSlides(slideIndex += 1);
-
+next.addEventListener("click", function () {
+  showSlides((slideIndex += 1));
 });
 
-prev.addEventListener ("click", function () {
-  showSlides(slideIndex -= 1);
+prev.addEventListener("click", function () {
+  showSlides((slideIndex -= 1));
 });
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
-
 function showSlides(n) {
-    let slides = document.getElementsByClassName("slide");
-    console.log("...........slides",slides)
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-
-    console.log('......length',slides.length)
-    let current = document.getElementsByClassName("active");
-
-    console.log(".........current",current)
-
-    if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-      }
-
-    slides[slideIndex - 1].className = slides[slideIndex - 1].className + ' active';    
+  let slides = document.getElementsByClassName("slide");
+  console.log("...........slides", slides);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
   }
 
- let timer = setInterval(function(){
-    slideIndex++;
-    showSlides(slideIndex);
-  },5000);
+  console.log("......length", slides.length);
+  let current = document.getElementsByClassName("active");
 
+  console.log(".........current", current);
+
+  if (current.length > 0) {
+    current[0].className = current[0].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].className =
+    slides[slideIndex - 1].className + " active";
+}
+
+let timer = setInterval(function () {
+  slideIndex++;
+  showSlides(slideIndex);
+}, 5000);
+
+const dropdownBtn = document.getElementById("btn-a");
+const dropdownMenu = document.getElementById("dropdown-a");
+const toggleArrow = document.getElementById("arrow");
+
+// Toggle dropdown function
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
+
+const dropdownBtn_c = document.getElementById("btn-c");
+const dropdownMenu_c = document.getElementById("dropdown-c");
+const toggleArrow_c = document.getElementById("arrow");
+
+// Toggle dropdown function
+const toggleDropdown_c = function () {
+  dropdownMenu_c.classList.toggle("show");
+  toggleArrow_c.classList.toggle("arrow");
+};
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn_c.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown_c();
+});
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu_c.classList.contains("show")) {
+    toggleDropdown_c();
+  }
+});
