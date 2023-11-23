728d13586e99686656d1de6c9ce7128230e29310
Pradeep <pradeep.kumar@qburst.com>
2023-11-23 14:46:46 +0530

    check3

--- script.js
+++ script.js
@@ -31,11 +31,18 @@
 // rightBtn.addEventListener("click", moveRight);
 // leftBtn.addEventListener("click", moveLeft);
 
-let slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener('DOMContentLoaded', function () {
+  // Add event listeners to buttons
+
 
 let prev = document.getElementById("prev");
 let next = document.getElementById("next");
+let slideIndex = 1;
+
+
+if (next)
+{
+  showSlides(slideIndex);
 
 next.addEventListener("click", function () {
   showSlides((slideIndex += 1));
@@ -52,12 +59,15 @@ function currentSlide(n) {
 function showSlides(n) {
   let slides = document.getElementsByClassName("slide");
   console.log("...........slides", slides);
+  if (slides)
+  {
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
+}
 
   console.log("......length", slides.length);
   let current = document.getElementsByClassName("active");
@@ -76,10 +86,11 @@ let timer = setInterval(function () {
   slideIndex++;
   showSlides(slideIndex);
 }, 5000);
+}
 
 const dropdownBtn = document.getElementById("btn-a");
 const dropdownMenu = document.getElementById("dropdown-a");
-const toggleArrow = document.getElementById("arrow");
+const toggleArrow = document.getElementById("arrow-a");
 
 // Toggle dropdown function
 const toggleDropdown = function () {
@@ -102,7 +113,7 @@ document.documentElement.addEventListener("click", function () {
 
 const dropdownBtn_c = document.getElementById("btn-c");
 const dropdownMenu_c = document.getElementById("dropdown-c");
-const toggleArrow_c = document.getElementById("arrow");
+const toggleArrow_c = document.getElementById("arrow-c");
 
 // Toggle dropdown function
 const toggleDropdown_c = function () {
@@ -122,3 +133,26 @@ document.documentElement.addEventListener("click", function () {
     toggleDropdown_c();
   }
 });
+
+function navigateTo(page) {
+  window.location.href = page;
+}
+
+
+  
+});
+
+
+
+
+// function loadContent(page) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//           document.getElementById('main-content').innerHTML = this.responseText;
+//       }
+//   };
+//   xhr.open('GET', page, true);
+//   xhr.send();
+// }
+
